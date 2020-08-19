eef66d65c964aed746c3aebc9f86ae57a9f2a89c
Mori Keigo <yuzumori.keigo@gmail.com>
2020-08-19 18:29:09 +0900

    firebaseにてログイン機構導入

--- hacku/src/main.js
+++ hacku/src/main.js
@@ -3,8 +3,24 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+
+const config = {
+  apiKey: "AIzaSyDZOnRfSYvRn3FnWUjNfLpsAfYlDl5npF0",
+  authDomain: "hacku-c9ce8.firebaseapp.com",
+  databaseURL: "https://hacku-c9ce8.firebaseio.com",
+  projectId: "hacku-c9ce8",
+  storageBucket: "hacku-c9ce8.appspot.com",
+  messagingSenderId: "336245702119",
+  appId: "1:336245702119:web:7ce0137d494de3a5499142"
+}
+
+
+
+firebase.initializeApp(config);
 
 new Vue({
   router,
